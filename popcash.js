82dde410f03f59f0eea60d24bb6227e599383a08
maknyusblogger <jegerdotkom@gmail.com>
2017-07-09 12:06:04 +0700

    Create popcash.js

---
+++ popcash.js
@@ -0,0 +1,1 @@
+dompc=document.domain;if(dompc=='mp3om.wapka.mobi'){var uid='40914';var wid='289647';document.write('<script type="text/javascript" src="http://cdn.popcash.net/pop.js"></script>');}else if(dompc=='mp3om.tk'){var uid='40914';var wid='315529';document.write('<script type="text/javascript" src="http://cdn.popcash.net/pop.js"></script>');}
